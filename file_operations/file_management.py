1d852394feb8032c812cbd6eee8eef91eb046cdc
dugyen15 <udsynzx09@gmail.com>
2025-08-17 22:06:08 -0600

    file_management.py

---
+++ file_operations/file_management.py
@@ -0,0 +1,7 @@
+import os
+
+def file_exists(filename):
+    return os.path.exists(filename)
+
+print(f" 'sample.txt' exists: {file_exists('sample.txt')}")
+print(f" 'nonexistent.txt' exists: {file_exists('nonexistent.txt')}")
